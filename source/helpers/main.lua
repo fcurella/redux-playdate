7d450c163af93b93e83acfd32deec6d86172bca6
Flavio Curella <89607+fcurella@users.noreply.github.com>
2022-11-23 22:47:57 +0000

    fix export

--- source/helpers/main.lua
+++ source/helpers/main.lua
@@ -2,7 +2,7 @@ local array = import("helpers/array")
 local assign = import("helpers/assign")
 local inspect = import("utils/inspect")
 
-local helpers = {
+return {
     Array = array,
     assign = assign,
     inspect = inspect,
